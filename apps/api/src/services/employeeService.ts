555d568e33265b83afebf6d8d93f36243719c582
sutarrohit <rohitsutar082@gmail.com>
2026-05-26 22:16:38 +0530

    feat: implement getEmployeeById  basic functionality

--- apps/api/src/services/employeeService.ts
+++ apps/api/src/services/employeeService.ts
@@ -1,5 +1,8 @@
+import { ApiError } from "@/src/lib/api-error.js";
 import { prisma } from "@/src/lib/prisma.js";
 import type { CreateEmployee } from "@/src/types/types.js";
+import { NOT_FOUND } from "stoker/http-status-codes";
+import { NOT_FOUND as NOT_FOUND_PHRASE } from "stoker/http-status-phrases";
 
 export async function createEmployee(data: CreateEmployee) {
   return await prisma.employee.create({
@@ -9,4 +12,12 @@ export async function createEmployee(data: CreateEmployee) {
       currency: data.currency ?? "USD"
     }
   });
+}
+
+export async function getEmployeeById(id: string) {
+  const employee = await prisma.employee.findUnique({ where: { id } });
+
+  if (!employee) throw new ApiError(NOT_FOUND, NOT_FOUND_PHRASE, "Employee not found");
+
+  return employee;
 }
